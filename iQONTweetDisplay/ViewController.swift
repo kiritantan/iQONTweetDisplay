98b8d721f9667f1f062830c3a03224e734dfa43c
kiri <conohatotama@google.com>
2016-06-14 19:43:53 +0900

    add selectTwitterAccount
    - get twitter acount information

--- iQONTweetDisplay/ViewController.swift
+++ iQONTweetDisplay/ViewController.swift
@@ -7,19 +7,45 @@
 //
 
 import UIKit
+import Accounts
+import Social
 
 class ViewController: UIViewController {
+    var accountStore = ACAccountStore()
+    var twAccount: ACAccount?
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.selectTwitterAccount()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    private func selectTwitterAccount() {
+        
+        let accountType = accountStore.accountTypeWithAccountTypeIdentifier(ACAccountTypeIdentifierTwitter)
+        
+        accountStore.requestAccessToAccountsWithType(accountType, options: nil) { (granted:Bool, error:NSError?) -> Void in
+            if error != nil {
+                print("error! \(error)")
+                return
+            }
+            
+            if !granted {
+                print("error! Twitterアカウントの利用が許可されていません")
+                return
+            }
+            
+            let accounts = self.accountStore.accountsWithAccountType(accountType) as! [ACAccount]
+            if accounts.count == 0 {
+                print("error! 設定画面からアカウントを設定してください")
+                return
+            }
+            self.twAccount = accounts[0]
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
-
