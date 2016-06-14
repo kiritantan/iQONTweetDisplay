41a29fd89eacf5696e8527162a221b9b4ab323a8
kiri <conohatotama@google.com>
2016-06-14 21:59:56 +0900

    implement Tweet

--- iQONTweetDisplay/Tweet.swift
+++ iQONTweetDisplay/Tweet.swift
@@ -7,3 +7,32 @@
 //
 
 import Foundation
+import UIKit
+
+class Tweet {
+    let fullname: String
+    let username: String
+    let tweetText: String
+    let timeStamp: String
+    var avatar = UIImage()
+    
+    init(fullname: String, username: String, avatarURLString: String, tweetText: String, timeStamp: String) {
+        self.fullname = fullname
+        self.username = username
+        self.tweetText = tweetText
+        self.timeStamp = timeStamp
+        self.avatar = downloadImageFromURL(avatarURLString)
+    }
+    
+    private func downloadImageFromURL(imageURLString: String) -> UIImage {
+        let url = NSURL(string: imageURLString.stringByReplacingOccurrencesOfString("\\", withString: ""))
+        let imageData = NSData(contentsOfURL: url!)
+        var image = UIImage()
+        if let data = imageData {
+            if let img = UIImage(data: data) {
+                image = img
+            }
+        }
+        return image;
+    }
+}
